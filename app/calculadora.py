c1c59bffb778533aa4f4c4cd155e723711e16cf5
sancho3010 <santiagohiguita@gmail.com>
2026-04-17 19:58:52 -0500

    Initial commit

---
+++ app/calculadora.py
@@ -0,0 +1,21 @@
+# app/calculadora.py
+
+AUTORES = "santiago.higuitau@eafit.edu.co"  # Reemplaza con tu correo EAFIT
+
+
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        raise ZeroDivisionError("No se puede dividir por cero")
+    return a / b
